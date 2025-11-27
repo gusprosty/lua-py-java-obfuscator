b2b2ecbde602f3c831ccfe731ac8b03de4a42a42
gusprosty <gusprosty1@gmail.com>
2025-11-27 23:12:05 +0300

    Create __init__.py

---
+++ obfuscators/__init__.py
@@ -0,0 +1,5 @@
+from .lua_obfuscator import LuaObfuscator
+from .python_obfuscator import PythonObfuscator
+from .java_obfuscator import JavaObfuscator
+
+__all__ = ['LuaObfuscator', 'PythonObfuscator', 'JavaObfuscator']
